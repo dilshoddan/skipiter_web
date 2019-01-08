e837484188e900871645882316bbcd839d6f6d73
Dilshod <dilshoddan@gmail.com>
2019-01-08 07:40:32 +0300

    Draft 1

---
+++ Sources/App/routes.swift
@@ -0,0 +1,35 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    
+    router.get("name") { req in
+        return "Dilshod Abdullaev"
+    }
+    
+    router.get("age") { req in
+        return 32
+    }
+}
+
+
+
+
+/* ORIGINAL TEMPLATE
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+ 
+ */
