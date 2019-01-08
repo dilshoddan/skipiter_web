9e3da77321f74087467cdc223798f2370ae7c61b
Dilshod <dilshoddan@gmail.com>
2019-01-08 18:31:07 +0300

    Add Package Auth 2.0.1

--- Package.swift
+++ Package.swift
@@ -11,10 +11,11 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
         //Vapor Leaf
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.1")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf", "Authentication"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
