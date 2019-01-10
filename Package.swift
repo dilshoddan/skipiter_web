0d0d9fa522a11ba8debdafce2e80e4301b159b57
Dilshod <dilshoddan@gmail.com>
2019-01-10 12:48:22 +0300

    "package changed"

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         
         //Vapor Leaf
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.1")
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf", "Authentication"]),
